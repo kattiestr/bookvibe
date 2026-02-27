54ca40bc812f81e5682c3193d93719ee141c1afc
kattiestr <138583101+kattiestr@users.noreply.github.com>
2026-02-27 14:27:10 -0500

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LibraryProvider } from './hooks/LibraryContext';
 import { CoverProvider } from './hooks/CoverContext';
 import { BooksProvider } from './hooks/BooksContext';
+import { AuthProvider, useAuth } from './hooks/AuthContext';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
@@ -17,41 +18,71 @@ import StatsPage from './pages/StatsPage';
 import AuthorPage from './pages/AuthorPage';
 import TBRPage from './pages/TBRPage';
 import NYTBookPage from './pages/NYTBookPage';
+import AuthPage from './pages/AuthPage';
+
+function AppRoutes() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div style={{
+        minHeight: '100vh',
+        backgroundColor: '#141010',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: '#e2ddd5',
+        fontSize: '18px',
+      }}>
+        Loading...
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <AuthPage />;
+  }
+
+  return (
+    <div style={{
+      backgroundColor: '#141010',
+      minHeight: '100vh',
+      color: '#e2ddd5',
+    }}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/book/:id" element={<BookPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/cycle" element={<CyclePage />} />
+        <Route path="/library" element={<LibraryPage />} />
+        <Route path="/library/:id" element={<LibraryBookPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/spin" element={<SpinPage />} />
+        <Route path="/if-you-liked" element={<IfYouLikedPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+        <Route path="/author/:name" element={<AuthorPage />} />
+        <Route path="/tbr" element={<TBRPage />} />
+        <Route path="/trending/:isbn" element={<NYTBookPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Navbar />
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <BooksProvider>
-      <CoverProvider>
-        <LibraryProvider>
-          <div
-            style={{
-              backgroundColor: '#141010',
-              minHeight: '100vh',
-              color: '#e2ddd5',
-            }}
-          >
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/book/:id" element={<BookPage />} />
-              <Route path="/favorites" element={<FavoritesPage />} />
-              <Route path="/cycle" element={<CyclePage />} />
-              <Route path="/library" element={<LibraryPage />} />
-              <Route path="/library/:id" element={<LibraryBookPage />} />
-              <Route path="/quiz" element={<QuizPage />} />
-              <Route path="/spin" element={<SpinPage />} />
-              <Route path="/if-you-liked" element={<IfYouLikedPage />} />
-              <Route path="/stats" element={<StatsPage />} />
-              <Route path="/author/:name" element={<AuthorPage />} />
-              <Route path="/tbr" element={<TBRPage />} />
-              <Route path="/trending/:isbn" element={<NYTBookPage />} />
-            </Routes>
-            <Navbar />
-          </div>
-        </LibraryProvider>
-      </CoverProvider>
-      </BooksProvider>
+      <AuthProvider>
+        <BooksProvider>
+          <CoverProvider>
+            <LibraryProvider>
+              <AppRoutes />
+            </LibraryProvider>
+          </CoverProvider>
+        </BooksProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
