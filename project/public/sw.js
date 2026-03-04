f8cf373b4138ba1648e085851e4c30f7560de206
kattiestr <138583101+kattiestr@users.noreply.github.com>
2026-03-04 18:39:12 -0500

    Update sw.js

--- project/public/sw.js
+++ project/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bookvibe-v1';
+const CACHE_NAME = 'bookvibe-v3';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -8,21 +8,10 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
   );
 });
 
@@ -30,9 +19,17 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
           .map(name => caches.delete(name))
       );
-    })
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
